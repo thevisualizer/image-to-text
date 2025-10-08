548159f69934e7f48cf6cbb561dfc2256f5e3e2d
achraf hakimi <121980406+thevisualizer@users.noreply.github.com>
2025-10-08 10:00:16 +0500

    Update php-request.php

--- php-request.php
+++ php-request.php
@@ -3,19 +3,18 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, [
-	CURLOPT_URL => "https://ocr-image-to-text4.p.rapidapi.com/image?etype=image",
+	CURLOPT_URL => "https://image-to-text40.p.rapidapi.com/ocr-by-image",
 	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_FOLLOWLOCATION => true,
 	CURLOPT_ENCODING => "",
 	CURLOPT_MAXREDIRS => 10,
 	CURLOPT_TIMEOUT => 30,
 	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 	CURLOPT_CUSTOMREQUEST => "POST",
-	CURLOPT_POSTFIELDS => "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"image\"\r\n\r\n\r\n-----011000010111000001101001--\r\n\r\n",
+	CURLOPT_POSTFIELDS => "",
 	CURLOPT_HTTPHEADER => [
-		"X-RapidAPI-Host: ocr-image-to-text4.p.rapidapi.com",
-		"X-RapidAPI-Key: YOUR-RAPID-API-KEY",
-		"content-type: multipart/form-data; boundary=---011000010111000001101001"
+		"Content-Type: application/x-www-form-urlencoded",
+		"x-rapidapi-host: image-to-text40.p.rapidapi.com",
+		"x-rapidapi-key: [YOUR-RAPID-API-KEY]"
 	],
 ]);
 
