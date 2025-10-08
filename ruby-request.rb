2248d67dcc11fbad541438c50b5a5b9dc970521f
achraf hakimi <121980406+thevisualizer@users.noreply.github.com>
2025-10-08 10:01:17 +0500

    Update ruby-request.rb

--- ruby-request.rb
+++ ruby-request.rb
@@ -1,18 +1,15 @@
 require 'uri'
 require 'net/http'
-require 'openssl'
 
-url = URI("https://ocr-image-to-text4.p.rapidapi.com/image?etype=image")
+url = URI("https://image-to-text40.p.rapidapi.com/ocr-by-image")
 
 http = Net::HTTP.new(url.host, url.port)
 http.use_ssl = true
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
 request = Net::HTTP::Post.new(url)
-request["content-type"] = 'multipart/form-data; boundary=---011000010111000001101001'
-request["X-RapidAPI-Key"] = 'YOUR-RAPID-API-KEY'
-request["X-RapidAPI-Host"] = 'ocr-image-to-text4.p.rapidapi.com'
-request.body = "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"image\"\r\n\r\n\r\n-----011000010111000001101001--\r\n\r\n"
+request["x-rapidapi-key"] = 'YOUR-RAPID-API-KEY'
+request["x-rapidapi-host"] = 'image-to-text40.p.rapidapi.com'
+request["Content-Type"] = 'application/x-www-form-urlencoded'
 
 response = http.request(request)
 puts response.read_body
